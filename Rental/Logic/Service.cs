7acbe0b4a2b7f518e6dc83bbba641882010c3a3e
s32962 <s32962@pjwstk.edu.pl>
2026-03-18 13:30:21 +0100

    Add: adding user and hardware to service, printing hardware with requested status ⠀⠀⠀⠀⠀⠀⡠⠛⠋⠉⠉⠓⠻⢤⠀ ⠀⠀⠀⠀⠀⡟⣠⣶⠖⣤⠀⣴⣯⣹⣷ ⠀⣀⣀⣀⠀⣆⢹⣿⣿⡿⠀⠹⠿⠿⣉ ⡞⠀⠀⠀⠀⠀⠓⠦⢄⡄⠩⠥⠔⠊⠁ ⠙⡄⢠⣴⣶⣦⠀⢠⡄⣴⠀⣯⠀⠀⠀ ⠀⠲⠖⠁⠉⠁⠧⠼⠁⠈⠓⠋⠀⠀⠀

--- Rental/Logic/Service.cs
+++ Rental/Logic/Service.cs
@@ -7,4 +7,44 @@ public class Service
     private List<Hardware> Hardwares { get; set; } = new();
     private List<User> Users { get; set; } = new();
     private List<Rental> Rentals { get; set; } = new();
+
+    public void AddUser(String name, String surname, USR_TYPE type)
+    {
+        int id = 0;
+        if(Users.Count != 0)
+            id = Users.Max(u => u.Id) + 1;
+        Users.Add(new User(id, name, surname, type));
+    }
+    public void AddHardware(Hardware hardware)
+    {
+        int id = 0;
+        if(Hardwares.Count != 0)
+            id = Hardwares.Max(h => h.Id) + 1;
+        hardware.Id = id;
+        Hardwares.Add(hardware);
+    }
+    public void PrintAllHardware()
+    {
+        Console.WriteLine($"{"ID",-5} {"Type",-10} {"Name",-20} {"Status",-15}");
+        Console.WriteLine(new string('-', 50));
+
+        foreach (Hardware h in Hardwares)
+        {
+            Console.WriteLine($"{h.Id,-5} {h.GetType().Name,-10} {h.Name,-20} {h.Status, -15}");
+        }
+        Console.WriteLine(new string('-', 50));
+    }
+    public void PrintAllHardware(STATUS status)
+    {
+        Console.WriteLine($"{"ID",-5} {"Type",-10} {"Name",-20} {"Status",-15}");
+        Console.WriteLine(new string('-', 50));
+
+        foreach (Hardware h in Hardwares)
+        {
+            if(h.Status == status)
+                Console.WriteLine($"{h.Id,-5} {h.GetType().Name,-10} {h.Name,-20} {h.Status, -15}");
+        }
+        Console.WriteLine(new string('-', 50));
+    }
+    
 }
--- Rental/Program.cs
+++ Rental/Program.cs
@@ -1,4 +1,5 @@
-﻿using Rental.Logic;
+﻿using Rental;
+using Rental.Logic;
 
 class Program
 {
@@ -6,5 +7,44 @@ class Program
     {
         Service service = new Service();
         
+        service.AddHardware(new LaptopBuilder()
+            .WithId(0)
+            .WithName("Dell XPS 15")
+            .WithPrice(1500)
+            .WithProcessor("Intel i7")
+            .WithGraphicsCard("RTX 3050")
+            .WithRAM(16)
+            .WithStorage(512)
+            .WithScreenSize(15.6)
+            .Build());
+
+        service.AddHardware(new LaptopBuilder()
+            .WithId(0)
+            .WithName("MacBook Pro")
+            .WithPrice(2500)
+            .WithProcessor("M2")
+            .WithRAM(16)
+            .WithStorage(1024)
+            .Build()); // partial specs okay
+        
+        service.AddHardware(new ProjectorBuilder()
+            .WithId(0)
+            .WithName("Epson X123")
+            .WithPrice(800)
+            .WithResolution("1080p")
+            .WithBrightness(3000)
+            .WithContrastRatio(10000)
+            .Build());
+        
+        service.AddHardware(new CameraBuilder()
+            .WithId(0)
+            .WithName("Canon EOS R5")
+            .WithPrice(3500)
+            .WithResolution(45)
+            .WithSensorType("Full Frame")
+            .WithLensMount("RF")
+            .Build());
+        
+        service.PrintAllHardware(STATUS.AVAILABLE);
     }
 }
